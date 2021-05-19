f32f2314a1937f090deed12ea226c86b72a1e6f6
Eberthyn <eberthyn@gmail.com>
2021-05-18 22:36:01 -0300

    Ajustes de Estilo (CSS)

--- assets/js/main.js
+++ assets/js/main.js
@@ -129,49 +129,6 @@ $(document).ready(function(){
       }, 500);
     });
 
-    if (tam <= 480) {
-      $("#inicioAula1").click(function () {
-        $('html, body').animate({
-          scrollTop: 1450
-        }, 500);
-      });
-
-      $("#inicioAula2").click(function () {
-        $('html, body').animate({
-          scrollTop: 1430
-        }, 500);
-      });
-
-      $("#inicioAula3").click(function () {
-        $('html, body').animate({
-          scrollTop: 1450
-        }, 500);
-      });
-
-      $("#inicioAula4").click(function () {
-        $('html, body').animate({
-          scrollTop: 1470
-        }, 500);
-      });
-
-      $("#inicioAula5").click(function () {
-        $('html, body').animate({
-          scrollTop: 1635
-        }, 500);
-      });
-
-      $("#inicioAula6").click(function () {
-        $('html, body').animate({
-          scrollTop: 1005
-        }, 500);
-      });
-
-      $("#inicioAula7").click(function () {
-        $('html, body').animate({
-          scrollTop: 1395
-        }, 500);
-      });
-    }
   } else {
     $("#inicioAula1").click(function () {
       $('html, body').animate({
@@ -216,3 +173,50 @@ $(document).ready(function(){
     });
 }
 });
+
+$(window).resize(function(){
+  var tam = $(window).width();
+if (tam <= 480) {
+  $("#inicioAula1").click(function () {
+    $('html, body').animate({
+      scrollTop: 1450
+    }, 500);
+  });
+
+  $("#inicioAula2").click(function () {
+    $('html, body').animate({
+      scrollTop: 1430
+    }, 500);
+  });
+
+  $("#inicioAula3").click(function () {
+    $('html, body').animate({
+      scrollTop: 1450
+    }, 500);
+  });
+
+  $("#inicioAula4").click(function () {
+    $('html, body').animate({
+      scrollTop: 1470
+    }, 500);
+  });
+
+  $("#inicioAula5").click(function () {
+    $('html, body').animate({
+      scrollTop: 1635
+    }, 500);
+  });
+
+  $("#inicioAula6").click(function () {
+    $('html, body').animate({
+      scrollTop: 1005
+    }, 500);
+  });
+
+  $("#inicioAula7").click(function () {
+    $('html, body').animate({
+      scrollTop: 1395
+    }, 500);
+  });
+}
+})
